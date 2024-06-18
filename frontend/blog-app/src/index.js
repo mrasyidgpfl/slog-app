6c6d63017e0d60b5254f3a5086f64b642ad1d45d
Rasyid Gatrawijaya <rasyid.wijaya@shopee.com>
2024-06-18 19:06:35 +0800

    chore(structure): Restructure Frontend repo

--- frontend/blog-app/src/index.js
+++ frontend/blog-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import theme from "./theme";
+import theme from "./themes/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
