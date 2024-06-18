69fb73ea906e9bb35b1e06b545ee45bb6b864fcc
Rasyid Gatrawijaya <rasyid.wijaya@shopee.com>
2024-06-18 15:27:00 +0800

    chore(lint): Configure ESLint with Google JavaScript style guide and integrate Prettier for code formatting

--- frontend/blog-app/src/App.js
+++ frontend/blog-app/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+/**
+ * @return {JDX.Element}
+ * Render the main application component.
+ */
 function App() {
+  const msg = "Hello, world!";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to my React App</h1>
+        <p>{msg}</p>
       </header>
     </div>
   );
