0b87d7c0430a490fec809ac40a9930d80d2e017d
Rasyid Gatrawijaya <rasyid.wijaya@shopee.com>
2024-06-21 01:06:27 +0800

    feat(backend): Initialize models.py for User, and Profile implementation

--- blog_project/slog_app/models.py
+++ blog_project/slog_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=160, blank=True)
+    join_date = models.DateTimeField(auto_now_add=True)
+    avatar = models.URLField(default='placeholder')
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
