b2d089390650f2586ab4da403d2fc25cc1ef043e
Rasyid Gatrawijaya <rasyid.wijaya@shopee.com>
2024-06-21 01:05:23 +0800

    feat(backend): Restart project, and set up settings.py

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -1,7 +1,8 @@
-"""blog_app URL Configuration
+"""
+URL configuration for blog_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('slog_app.urls')),  # Include app-specific URLs
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
