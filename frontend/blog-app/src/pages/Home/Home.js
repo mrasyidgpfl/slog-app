bbf4bc8e09a84c1df7af6ded7b747de8fa58142f
Rasyid Gatrawijaya <rasyid.wijaya@shopee.com>
2024-06-26 19:41:42 +0800

    feat(frontend): Add token refresher implementations to other pages

--- frontend/blog-app/src/pages/Home/Home.js
+++ frontend/blog-app/src/pages/Home/Home.js
@@ -1,18 +1,49 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Box, Container } from "@mui/material";
+import { Grid, Box, Container, Snackbar } from "@mui/material";
 import BlogPost from "../Blog/BlogPost";
 import { fetchBlogPosts } from "../../services/blogs";
+import { useSelector, useDispatch } from "react-redux";
+import { refreshAccessTokenAction } from "../../redux/actions/authActions";
+import { isTokenExpired } from "../../utils/authUtils";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+  const { accessToken, refreshToken } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchBlogPosts();
-      setPosts(data);
+      try {
+        const data = await fetchBlogPosts(accessToken);
+        setPosts(data);
+      } catch (error) {
+        console.error("Error fetching blog posts:", error);
+        setError("Failed to fetch blog posts");
+      }
     };
+
     fetchData();
-  }, []);
+  }, [accessToken]);
+
+  useEffect(() => {
+    const refreshIfNeeded = async () => {
+      if (isTokenExpired(accessToken)) {
+        try {
+          await dispatch(refreshAccessTokenAction(refreshToken));
+        } catch (error) {
+          console.error("Error refreshing access token:", error);
+          // Handle error refreshing access token (e.g., log out user)
+        }
+      }
+    };
+
+    refreshIfNeeded();
+  }, [accessToken, refreshToken, dispatch]);
+
+  const handleSnackbarClose = () => {
+    setError(null); // Clear error state when Snackbar closes
+  };
 
   return (
     <Container
@@ -38,6 +69,13 @@ const Home = () => {
           ))}
         </Grid>
       </Box>
+      <Snackbar
+        open={!!error}
+        autoHideDuration={2000}
+        onClose={handleSnackbarClose}
+        message={`Error: ${error}`}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      />
     </Container>
   );
 };
